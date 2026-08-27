9558824e6765c674a078103740fedc88845c895b
Parag M. Piprewar <92796320+ParagRider1@users.noreply.github.com>
2026-08-27 10:17:48 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string news="";
+        // or to transform to lower case
+        // transform(s.begin(),s.end(),s.begin(),[](char &c){
+        //     return tolower(c);
+        // });
+       
+        for(int i=0;i<s.size();i++){
+            if(s[i]>=65 && s[i]<=90){
+                news+=(char)((s[i]-'A')+97); //lowercase
+            }
+            else if(s[i]==' '){
+                continue;
+            } else if( (s[i]>=97 && s[i]<=122) || (s[i]-'0'>=0 && s[i]-'0'<=9)){
+                news+=s[i];
+            }   
+        }
+        int st=0;int end=news.size()-1;
+        while(st<=end){
+            if(news[st]!=news[end]){
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+        // string revnews=news;
+        // reverse(revnews.begin(),revnews.end());
+
+        // if(news==revnews){
+        //     return true;
+        // }
+        // return false;
+    }
+};
