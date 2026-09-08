fcb3db1f324307d89e71312e6e7636ec43b65881
Parag M. Piprewar <92796320+ParagRider1@users.noreply.github.com>
2026-09-08 16:55:42 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        //rotating 90* inplace == transpose(by swapping i,j with j,i in LTM) + reversing each rows 
+        int n=mat.size();//rows
+        int m=mat[0].size();//col
+
+
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+
+        //reversing each row
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+
+    }
+};
